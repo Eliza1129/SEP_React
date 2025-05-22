9eb0b09cb379566b39ee4b45bfaba0017287c9ed
Eliza1129 <139704196+Eliza1129@users.noreply.github.com>
2025-05-22 00:13:56 -0700

    Update useSelector.js

--- redux/src/Context/useSelector.js
+++ redux/src/Context/useSelector.js
@@ -1,15 +1,23 @@
-import {useContext, useEffect, useState} from 'react';
+import {useContext, useEffect, useState, useRef} from 'react';
 import {StoreContext} from "./StoreContext.jsx";
 
 
-function useSelector(selectorFn) {
+const useSelector = (selectorFn) => {
     const store = useContext(StoreContext);
+
+    if (!store || typeof store.getState !== "function") {
+        throw new Error("useSelector must be used inside a <StoreProvider>");
+    }
     const [selectedState, setSelectedState] = useState(() => selectorFn(store.getState()));
+    const latestSelectedRef = useRef(selectedState);
 
     useEffect(() => {
         const unsubscribe = store.subscribe(() => {
             const newSelected = selectorFn(store.getState());
-            setSelectedState(newSelected);
+            if(newSelected !== latestSelectedRef.current) {
+                latestSelectedRef.current = newSelected;
+                setSelectedState(newSelected);
+            }
         });
 
         return unsubscribe;
